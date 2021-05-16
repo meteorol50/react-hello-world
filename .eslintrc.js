ed0d4231296c8bd542b8a809a7e1eabbfefd1ce4
meteorol50 <meteorol50.sky@gmail.com>
2021-05-16 18:35:28 +0900

    ESLint導入

---
+++ .eslintrc.js
@@ -0,0 +1,132 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // 各ESLintプラグインルールの推奨の共有設定、ESLintプラグイン開発者が一緒に提供している、順番大事
+  // ESLintプラグインをインストールしてここでのルール有効化が必要
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    // プロジェクトのTSコンパイル設定ファイルのパス設定
+    // @typescript-eslint/recommended-requiring-type-checkingなど型情報が要求されるルールを使用したい場合に必要
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  // 読み込ませる追加ルールのプラグイン、ここも記載によって有効化が必要
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    '@emotion',
+  ],
+  // ESLintが親ディレクトリの設定ファイルまで読み込んでしまうのを阻止
+  root: true,
+  rules: {
+    // occur error in `import React from 'react'` with react-scripts 4.0.3
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    'no-void': [
+      'error',
+      {
+        allowAsStatement: true,
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.stories.{js,jsx,ts,tsx}'],
+        peerDependencies: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '_',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '_',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    '@emotion/jsx-import': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+    {
+      files: ['**/*.stories.{js,jsx,ts,tsx}'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
