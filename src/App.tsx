fb163a07041d0fe161a28eb34bd12e9045635f25
meteorol50 <60614541+meteorol50@users.noreply.github.com>
2021-05-17 18:54:27 +0900

    create diff

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: FC = () => (
       <p>
         Edit
         <code>src/App.tsx</code>
-        and save to reload !!
+        and save to reload !
       </p>
       <a
         className="App-link"
