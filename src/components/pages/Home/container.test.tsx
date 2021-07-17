f29841eef505f2584d52bae0de5001312fda8f6c
meteorol50 <meteorol50.sky@gmail.com>
2021-07-17 14:26:24 +0900

    create First Test

---
+++ src/components/pages/Home/container.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import EnhancedHome from './container';
+
+jest.mock('.', () => ({ isShow }: { isShow: boolean }) => <div data-testid="isShow">{isShow ? 'true' : 'false'}</div>);
+
+describe('Home', () => {
+  it('has no text', async () => {
+    const history = createMemoryHistory();
+    const { findByTestId } = render(
+      <Router {...{ history }}>
+        <EnhancedHome />
+      </Router>,
+    );
+    const isShowDiv = await findByTestId('isShow');
+    expect(isShowDiv.innerHTML).toEqual('false');
+  });
+
+  it('has a text', async () => {
+    const history = createMemoryHistory();
+    history.push('/?category=1');
+    const { findByTestId } = render(
+      <Router {...{ history }}>
+        <EnhancedHome />
+      </Router>,
+    );
+    const isShowDiv = await findByTestId('isShow');
+    expect(isShowDiv.innerHTML).toEqual('true');
+  });
+});
